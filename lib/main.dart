4f7a9de055763caf3d1934e4497d38d0af87967b
dao the linh <daothelinh80@gmail.com>
2022-06-22 10:40:05 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:aicycle_login_seta/screen/auth_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: const MyHomePage(),
+      //home: const AuthPage(),
     );
   }
 }
---
+++ lib/screen/auth_page.dart
@@ -0,0 +1,2 @@
+import 'package:aicycle_login_seta/main.dart';
+import 'package:flutter/material.dart';
